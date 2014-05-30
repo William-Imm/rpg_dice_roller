94f3093cdb3612c64483f3b2f8e32ca494968dc3
William Immendorf <will.immendorf@gmail.com>
2014-05-30 11:45:58 -0500

    Add basic documentation to the program.

--- lib/rpg_dice_roller.rb
+++ lib/rpg_dice_roller.rb
@@ -1,6 +1,8 @@
 require 'rpg_dice_roller/version'
 require 'rpg_dice_roller/dice'
 
+# This is the main module for rpg_dice_roller.
+# This may play more of an active role in the future, but it's currently empty of code.
 module RpgDiceRoller
   # Your code goes here...
 end
--- lib/rpg_dice_roller/dice.rb
+++ lib/rpg_dice_roller/dice.rb
@@ -1,4 +1,13 @@
 module RpgDiceRoller
+  # Represents a die with a variable number of sides.
+  # The die can be instated with a number of sides, represented by a number, or by a d-notation string.
+  #---
+  # However, it might be better to move the regex dice processing to another part, and just have the dice take in a hash.
+  #+++
+  # Example:
+  #   die = RpgDiceRoller::Dice.new(20)
+  #   die.sides   #=> 20
+  #   die.roll    #=> random number between 1 and 20
   class Dice
     attr_accessor :sides
 
@@ -10,7 +19,9 @@ module RpgDiceRoller
         @sides = match_array[2].to_i
       end
     end
-
+    
+    # Rolls the dice.
+    # This returns a random number between 1 and @sides.
     def roll
       rand(sides) + 1
     end
