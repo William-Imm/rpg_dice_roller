9dc0d020259416fd3ccb0df5bc74373ff89c76c4
William Immendorf <will.immendorf@gmail.com>
2014-05-29 20:51:18 -0500

    step 0: add scaffolding for existing cucumber steps

---
+++ features/step_definitions/dice.steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have a die of "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I roll the die$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a result above "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a result below "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
