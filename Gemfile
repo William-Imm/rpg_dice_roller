0240bf2ed33a3afb4252e1a157c013d3206d6b65
William Immendorf <will.immendorf@gmail.com>
2014-05-30 19:48:47 -0500

    remove Spork, not needed

--- Gemfile
+++ Gemfile
@@ -2,11 +2,9 @@ source 'https://rubygems.org'
 
 group :development do
   gem 'rubocop', '~> 0.22.0'
-  gem 'spork', '~> 0.9.2', require: false
   gem 'guard-rspec', '~> 4.2.9'
   gem 'guard-cucumber', '~> 1.4.1'
   gem 'guard-bundler', '~> 2.0.0'
-  gem 'guard-spork', '~> 1.5.1'
   gem 'guard-rubocop', '~> 1.1.0'
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,28 +1,2 @@
 require 'rubygems'
-begin
-  require 'spork'
-rescue LoadError
-  # rubocop:disable Documentation
-  module Spork
-    def self.prefork
-      yield
-    end
-
-    def self.each_run
-      yield
-    end
-  end
-  # rubocop:enable Documentation
-end
-
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-
-end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  require 'rpg_dice_roller'
-end
+require 'rpg_dice_roller'
